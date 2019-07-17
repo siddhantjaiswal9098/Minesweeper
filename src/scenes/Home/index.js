899720f75d9dd19a2ed634945032919b4dc858ac
siddhantjaiswal <30575892+siddhantjaiswal@users.noreply.github.com>
2019-07-18 02:13:20 +0530

    code cleaning and optimisations

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
 
 const Home = () => {
 
@@ -15,13 +14,11 @@ const Home = () => {
     for (var i = 0; i < x.length; i++) {
       x[i] = new Array(valueParsed).fill(0);
     }
-    console.log(x);
     setInputValue(valueParsed)
     setArray(x)
   }
   const checkMinesweeper = () => {
     let resultArray = JSON.parse(JSON.stringify(array));
-    console.log('output', resultArray)
     for (let i = 0; i < array.length; i++) {
       for (let j = 0; j < array[i].length; j++) {
         if (array[i][j] === 1) {
@@ -31,7 +28,7 @@ const Home = () => {
           // loop through all 8 elements around the mine.
           for (let adjecentValuesx = -1; adjecentValuesx <= 1; adjecentValuesx++) {
             for (let adjecentValuesy = -1; adjecentValuesy <= 1; adjecentValuesy++) {
-              if (!(adjecentValuesx == 0 && adjecentValuesy == 0)) {
+              if (!(adjecentValuesx === 0 && adjecentValuesy === 0)) {
                 if (array[i + adjecentValuesx] !== undefined && array[i + adjecentValuesx][j + adjecentValuesy] !== undefined) {
                   arrayOfValuesNearNonMines.push(array[i + adjecentValuesx][j + adjecentValuesy]);
                 }
@@ -48,20 +45,19 @@ const Home = () => {
 
   const changeValue = (index, index2) => {
     array[index][index2] = 1;
-    console.log('arrrayyyyyy', array)
     setArray([...array]);
   }
   return (
     <div style={{ padding: 20 }}>
       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Button color="primary" style={{ padding: 20 }} disabled={!inputValue} onClick={() => generateArray(-1)}>-</Button>
+        <Button color="primary" style={{ padding: '7px 30px 7px 30px' }} disabled={!inputValue} onClick={() => generateArray(-1)}>-</Button>
         <span style={{ fontSize: 25 }}>{inputValue}</span>
-        <Button color="primary" style={{ padding: 20 }} onClick={() => generateArray(1)}>+</Button>
+        <Button color="primary" style={{ padding: '7px 30px 7px 30px' }} onClick={() => generateArray(1)}>+</Button>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div>
-          <div for="exampleEmail">Minesweeper</div>
-          <Button color="primary" style={{ padding: 20 }} disabled={!inputValue} onClick={() => checkMinesweeper()}>SUBMIT</Button>
+          <div>Minesweeper</div>
+          <Button color="primary" style={{ padding: '7px 30px 7px 30px' }} disabled={!inputValue} onClick={() => checkMinesweeper()}>SUBMIT</Button>
         </div>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', padding: 50 }}>
@@ -70,8 +66,8 @@ const Home = () => {
             array.map((obj, index) => {
               return obj.map((obj2, index2) => {
                 if (obj.length === index2 + 1) {
-                  return <><span onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span><br /></>
-                } else return <span onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span>
+                  return <span key={index2}><span onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span><br /></span>
+                } else return <span key={index2} onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span>
               })
             })
           }
@@ -84,8 +80,8 @@ const Home = () => {
             resultArray.map((obj, index) => {
               return obj.map((obj2, index2) => {
                 if (obj.length === index2 + 1) {
-                  return <><span onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span><br /></>
-                } else return <span onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span>
+                  return <span key={index2}><span onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span><br /></span>
+                } else return <span key={index2} onClick={() => changeValue(index, index2)} style={{ padding: '1px 18px 1px 18px', border: '1px solid black' }}>{obj2}</span>
               })
             })
           }
@@ -94,14 +90,5 @@ const Home = () => {
     </div>
   );
 }
-
-Home.defaultProps = {
-  comments: [{}],
-};
-Home.propTypes = {
-  comments: PropTypes.array,
-  getCommentsFunction: PropTypes.func.isRequired,
-  commentsLoading: PropTypes.bool.isRequired,
-};
 
 export default Home;
