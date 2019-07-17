e2fbf1685628369fe8df09c0bc6efef2e3b262df
siddhantjaiswal <30575892+siddhantjaiswal@users.noreply.github.com>
2019-07-18 00:42:22 +0530

    generate array with the values and show it on the UI also click event for every span done

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -3,32 +3,50 @@ import PropTypes from 'prop-types';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Home = () => {
+  
   const [inputValue, setInputValue] = useState(0)
-  var x = new Array(inputValue);
-  for (var i = 0; i < x.length; i++) {
-    x[i] = new Array(inputValue).fill('0');
+
+  const [array, setArray] = useState([]);
+
+  const generateArray = (value) => {
+    const valueParsed = parseInt(inputValue)+value
+    var x = new Array(valueParsed);
+    for (var i = 0; i < x.length; i++) {
+      x[i] = new Array(valueParsed).fill('0');
+    }
+    console.log(x);
+    setInputValue(valueParsed)
+    setArray(x)
   }
-  console.log(x);
+
+  const changeValue = (index, index2) => {
+    array[index][index2] = '1'
+    console.log('arrrayyyyyy', array)
+    setArray([...array]);
+  }
   return (
       <div style={{padding: 20}}>
         <FormGroup>
           <Label for="exampleEmail">Minesweeper</Label>
-          <div style={{display: 'flex', justifyContent: 'space-between'}}>
-            <Button disabled={!inputValue} onClick={() => setInputValue(parseInt(inputValue)-1)}>-</Button>
-                <span>{inputValue}</span>
-            <Button onClick={() => setInputValue(parseInt(inputValue)+1)}>+</Button>
+          <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <Button style={{padding: 20}} disabled={!inputValue} onClick={() => generateArray(-1)}>-</Button>
+                <span style={{fontSize: 25}}>{inputValue}</span>
+            <Button style={{padding: 20}} onClick={() => generateArray(1)}>+</Button>
           </div>
         </FormGroup>
-        <div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <div>
         {
-          x.map((obj) => {
-            return obj.map((obj2, index) => {
-              if(obj2.length === index ) {
-                return <span><span>0</span><br /></span>
-              } else return <span>0</span>
+          array.map((obj, index) => {
+            return obj.map((obj2, index2) => {
+              // console.log('obj2obj2obj2', obj.length, index2)
+              if(obj.length === index2 + 1 ) {
+                return <><span onClick={() => changeValue(index, index2)} style={{padding: '1px 18px 1px 18px', border: '1px solid black'}}>{obj2}</span><br /></>
+              } else return <span onClick={() => changeValue(index, index2)} style={{padding: '1px 18px 1px 18px', border: '1px solid black'}}>{obj2}</span>
             })
           })
         }
+        </div>
         </div>
       </div>
     );
